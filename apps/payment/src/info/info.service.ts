0b3c38aafb7cf973a52f4309f11f844bcfa374f1
broisnischal <info@nischal-dahal.com.np>
2025-01-28 15:10:55 +0545

    feat:init

---
+++ apps/payment/src/info/info.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { YcI18nService } from '../yc-i18n/yc-i18n.service';
+
+@Injectable()
+export class InfoService {
+  constructor(private readonly i18n: YcI18nService) {}
+
+  getInfo() {
+    return this.i18n.t('common.about');
+  }
+}
---
+++ apps/payment/src/payment.module.ts
@@ -0,0 +1,39 @@
+import { Module } from '@nestjs/common';
+import { PaymentController } from './payment.controller';
+import { PaymentService } from './payment.service';
+import { InfoModule } from './info/info.module';
+import {
+  AcceptLanguageResolver,
+  HeaderResolver,
+  I18nModule,
+  I18nYamlLoader,
+} from 'nestjs-i18n';
+import { YcI18nModule } from './yc-i18n/yc-i18n.module';
+import * as path from 'node:path';
+
+@Module({
+  imports: [
+    InfoModule,
+    I18nModule.forRoot({
+      fallbackLanguage: 'en',
+      loader: I18nYamlLoader,
+
+      loaderOptions: {
+        path: path.join(__dirname, '/locales/'),
+        watch: true,
+      },
+      resolvers: [
+        AcceptLanguageResolver,
+        { use: HeaderResolver, options: ['x-lang'] },
+      ],
+      typesOutputPath: path.join(
+        __dirname,
+        '../src/generated/i18n.generated.ts',
+      ),
+    }),
+    YcI18nModule,
+  ],
+  controllers: [PaymentController],
+  providers: [PaymentService],
+})
+export class PaymentModule {}
